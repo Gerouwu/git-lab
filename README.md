a488347194c59bd2cb202816de679876de69db56
Geronimo <viejogero@gmail.com>
2026-09-11 16:14:00 -0500

    Create endpoint version and modifies README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+#FastaAPI Git Lab 
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = FastAPI( )
 @app.get("/health")
 def health():
     return {"status":  "ok"}
-
+@app.get("/version")
+def version():
+    return {"Version": "1.0.0"}
 @app.get("/author")
 def author():
     return {"Author": "Juan"}
